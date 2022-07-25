d978a522f5a445893c468386e1445d5661778afe
nora <anes2354@gmail.com>
2022-07-25 18:44:58 +0900

    init

---
+++ OutOfMemory/Shared/OutOfMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutOfMemoryApp.swift
+//  Shared
+//
+//  Created by TOBO WORLD on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct OutOfMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
